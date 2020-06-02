425f4963264493a272787da7a2de8eb96bdbdda4
audrianalinares <audriana_linares@yahoo.com>
2020-06-02 17:53:56 -0500

    added components

---
+++ react-project/src/TodoItem/TodoItem.js
@@ -0,0 +1,11 @@
+import Teact from 'react';
+
+class TodoItem extends React.Component {
+
+    render() {
+        return(<div>list component</div>);
+    }
+
+
+}
+export default TodoItem;
---
+++ react-project/src/TodoList/TodoList.js
@@ -0,0 +1,11 @@
+import Teact from 'react';
+
+class TodoList extends React.Component {
+
+    render() {
+        return(<div>list component</div>);
+    }
+
+
+}
+export default TodoList;
